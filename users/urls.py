a409a4d356095b653e98a432cdf3a39ad320c3a2
likikikpa <144997323+likikikpa@users.noreply.github.com>
2023-09-14 11:54:24 +0300

    first commit

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+"""Определяет схемы url для пользователей """
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # Включить url авторизации по умолчанию
+    path('', include('django.contrib.auth.urls')),
+    # Страница регистрации
+    path('register/', views.register, name='register')
+]
